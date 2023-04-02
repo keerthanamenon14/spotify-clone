a240e2c44a4fa08e5072b94eb13a76a1bbd05400
BEO\keerthana.r <keerthana.r@beo.in>
2023-04-02 14:54:47 +0530

    implemented artist details

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -7,9 +7,16 @@ const AroundYou = () => {
     const [country,setCountry] = useState('')
     const [loading,setLoading] = useState(true)
     const {isActiveSong, isPlaying} = useSelector((state) => state.player)
-
+    console.log(country);
     useEffect(()=>{
-
+        axios.get(`https://api.geoapify.com/v1/ipinfo?apiKey=065c3f5c7a6e42f8abc64cfb9a918aec`)
+        .then(
+            (res) => setCountry(res?.data.country.name)
+        ).catch(
+            (error) => console.log(error)
+        ).finally(
+            () => setLoading(false)
+        )
     },[country])
 
     return(
